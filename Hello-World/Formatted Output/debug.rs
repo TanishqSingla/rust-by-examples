18aa6f4451da26e83ac706143168e0311a9bf64e
TanishqSingla <tanishqsingla00@gmail.com>
2020-11-11 01:32:50 +0530

    Adding formatted output

---
+++ Hello-World/Formatted Output/debug.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // In general `{}` is used as a placeholder in strings to replace it with a value
+
+    //Example
+    println!("Novemeber has {} days", 30);
+
+    // You can store a number in `{}` like this `{1}` to refer the position of a value
+
+    //Example
+    println!("{0} this is {0}, this is {1}", "Alice", "Bob");
+}

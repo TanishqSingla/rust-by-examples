8a394ed96531df635acec9a4cb575604a35e978e
TanishqSingla <tanishqsingla00@gmail.com>
2020-11-29 17:57:30 +0530

    Deriving debug format for functions

---
+++ Hello-World/Formatted Output/debug.rs
@@ -0,0 +1,14 @@
+#[derive(Debug)]
+struct Structure(i32);
+
+#[derive(Debug)]
+struct Deep(Structure); 
+
+fn main() {
+    //Printing with `{:?}` is similar to with `{}`
+    println!("{:?} months in a year", 12);
+    println!("{1:?} {0:?} is the {actor:?} name",
+             "Slater", 
+             "Christian", 
+             actor="actor's");
+}

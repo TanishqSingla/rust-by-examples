cc81c4bccadc9af538f2d41c28b648a70ec632ab
TanishqSingla <tanishqsingla00@gmail.com>
2020-12-25 16:35:09 +0530

    Adding display

---
+++ Hello-World/Formatted Output/Display/display.rs
@@ -0,0 +1,10 @@
+use std::fmt;
+
+
+// defining a structure on which we will implement fmt::display
+struct Structure(i32);
+
+// Implementing fmt::display for Structure
+impl fmt::Display for Structure {
+    
+}

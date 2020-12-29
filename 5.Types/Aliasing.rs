b1f225a493f985a797e14fc8aeae40d9b3d55269
TanishqSingla <tanishqsingla00@gmail.com>
2020-12-30 01:03:02 +0530

    Added aliasing

---
+++ 5.Types/Aliasing.rs
@@ -0,0 +1,21 @@
+// type aliases must have UpperCamelCase names or compiler will raise warning
+type NanoSecond = u64;
+type Inch = u64;
+
+// Use an attribute to silence warning
+#[allow(non_camel_case_types)]
+type u64_t = u64;
+// TODO ^ Try removing the attribute
+
+fn main() {
+  // `NanoSecond` = `Inch` = `u64_t` = `u64`
+  let nanosecond: NanoSecond = 5 as u64_t;
+  let inches: Inch = 2 as u64_t;
+
+  // Note type aliases don't provide extra type safety, because
+  // aliases are not new types
+  println!("{} nanoseconds + {} inches = {} unit?",
+            nanosecond,
+            inches,
+            nanosecond + inches);
+}

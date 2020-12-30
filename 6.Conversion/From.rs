2cb215325e22faadf50b25be398f1414e9bf62b1
TanishqSingla <tanishqsingla00@gmail.com>
2020-12-30 22:12:24 +0530

    Added from.rs

---
+++ 6.Conversion/From.rs
@@ -0,0 +1,23 @@
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+  value: i32,
+}
+
+impl From<i32> for Number {
+  fn from(item: i32) -> Self {
+    Number {value: item} 
+  }
+}
+
+fn main() {
+  // general example of `from` trait
+  // This example converts an `str` to a String
+  let my_str = "hello";
+  let _  my_string = String::from(my_str);
+
+  let num = Number::from(30);
+  println!("My number is {:?}", num);
+}
+

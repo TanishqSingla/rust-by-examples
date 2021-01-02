76129a0d3916f02ba8fbbcf0a082e9629fa86f80
TanishqSingla <tanishqsingla00@gmail.com>
2021-01-02 22:22:00 +0530

    Added while-let

---
+++ 7.Flow-of-Control/while-let.rs
@@ -0,0 +1,36 @@
+#[allow(dead_code)]
+
+fn _unused_code() {
+  let mut optional = Some(0);
+
+  // Repeatedly try this test
+  loop {
+    match optional {
+      // If `optional` destructures, evaluate the block.
+      Some(i) => {
+        if i > 9 {
+          println!("Greater than 9, quit!");
+          optional = None;
+        } else {
+          println!("`i` is `{:?}`. Try again.", i);
+          optional = Some(i + 1);
+        }
+      },
+      _ => {break;}
+    }
+  }
+}
+
+fn main() {
+  let mut optional = Some(0);
+
+  while let Some(i) = optional {
+    if i > 9 {
+      println!("Greater than 9, quit!");
+      optional = None;
+    } else {
+      println!("`i` is `{:?}`. Try again!");
+      optional = Some(i + i);
+    }
+  }
+}

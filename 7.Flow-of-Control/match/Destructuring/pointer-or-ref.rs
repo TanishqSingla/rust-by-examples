b247f6106af1b8583783dce12fbcd1c49eee3e27
TanishqSingla <tanishqsingla00@gmail.com>
2021-01-02 20:19:46 +0530

    Added ref mut and ref

--- 7.Flow-of-Control/match/Destructuring/pointer-or-ref.rs
+++ 7.Flow-of-Control/match/Destructuring/pointer-or-ref.rs
@@ -19,4 +19,33 @@ fn main() {
   match *reference {
     val => println!("Got a value via dereferencing: {:?}", val),
   }
+
+  // What if you didn't start with a refernce? `reference` was a `&
+  // because the right side was already a reference. This is not
+  // a reference because the right side is not one.
+  let _not_a_reference = 3;
+
+  // Rust provides `ref` for exactly this purpose. It modifies the
+  // assignment so that a reference is created for a element; this
+  // reference is assigned
+  let ref _is_a_reference = 3;
+
+  // Accordingly, by defining 2 values without a references, references
+  // can be retrieved via `ref` and `ref mut`.
+  let value = 5;
+  let mut mut_value = 6;
+
+  // Using keyword `ref` to create a reference
+  match value {
+    ref r => println!("Got a reference to a value: {:?}", r),
+  }
+
+  // Using `ref mut` similarly.
+  match mut_value {
+    ref mut m => {
+      // Got a reference, so we dereference here
+      *m += 10;
+      println!("We added 10. `mut_value`: {:?}", m);
+    },
+  }
 }

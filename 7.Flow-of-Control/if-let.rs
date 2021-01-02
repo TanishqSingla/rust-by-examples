f98fac21ffd0aea63e568d8ce671b8f64edc25ce
TanishqSingla <tanishqsingla00@gmail.com>
2021-01-02 22:15:39 +0530

    added enum example in if let

--- 7.Flow-of-Control/if-let.rs
+++ 7.Flow-of-Control/if-let.rs
@@ -17,7 +17,7 @@ fn _unused_example() {
 
 // if let cleaner for this use and in addition allows various failiure options to be specified
 
-fn main() {
+fn _example_main() {
   // ALl have type `Option<i32>`
   let number = Some(7);
   let letter: Option<i32> = None;
@@ -45,5 +45,39 @@ fn main() {
     println!("Didn't match a number, Let's go with a letter!");
   } else {
     println!("I don't like letters, Let's go with an emoticon");
+  }
+}
+
+// Our example enum
+enum Foo {
+  Bar,
+  Baz,
+  Qux(u32),
+}
+
+fn main() {
+  let a = Foo::Bar;
+  let b = Foo::Baz;
+  let c = Foo::Qux(100);
+
+  // Variable a matches Foo::Bar
+  if let Foo::Bar = a {
+    println!("a is a foobar");
+  }
+
+  // Variable b doesn't matches so it will not print
+  if let Foo::Bar = b {
+    println!("b is foobar");
+  }
+
+  // Variable c matches Foo::Qux which has a value
+  // Similar to Some() in the previous example
+  if let Foo::Qux(value) = c {
+    println!("c is {}", value);
+  }
+
+  // Binding also works with `if let`
+  if let Foo::Qux(value @ 100) = c {
+    println!("c is {}", value);
   }
 }

8d1f507d8d1afa6de2319dba0b5dad1f263c2fff
TanishqSingla <tanishqsingla00@gmail.com>
2021-01-05 13:52:38 +0530

    Added use declaration

---
+++ 9.Modules/use-declaration.rs
@@ -0,0 +1,32 @@
+// `as` can be used to give different names in import
+use deeply::nested::function as other_function;
+
+fn function() {
+  println!("called `function()`");
+}
+
+mod deeply {
+  pub mod nested {
+    pub fn function() {
+      println!("called `deeply::nested::function()`");
+    }
+  }
+}
+
+fn main() {
+  other_function();
+
+  println!("Entering block");
+  {
+    // This is equivalent to `use::deeply::nested::function as function`
+    // This function() will shoadow the outer one.
+    use crate::deeply::nested::function;
+
+    // `use` bindings have local scope
+    function();
+
+    println!("Leaving Block");
+  }
+
+  function();
+}

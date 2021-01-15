9bd4a0d6c2947e71180f25c0aeaa036629e1d3a0
TanishqSingla <tanishqsingla00@gmail.com>
2021-01-15 19:04:02 +0530

    added non utf8 strings

--- 17.Std-library-types/strings.rs
+++ 17.Std-library-types/strings.rs
@@ -1,3 +1,5 @@
+use std::str;
+
 fn main() {
     // (all the type annotations are superfluous)
     // A reference to a string allocated in read only memory
@@ -68,4 +70,36 @@ fn main() {
     // There is no limit for the number of #s you can use.
     let long_delimiter = r###"A string with "# in it. And even"##!"###;
     println!("{}", long_delimiter);
+
+    let bytestring: &[u8; 21] = b"this is a byte string";
+
+    // Byte arrays don't have the `Display` trait so printing them is a bit limited
+    println!("A byte string: {:?}", bytestring);
+
+    // Byte strings can have byte escapes...
+    let escaped = b"\x52\x75\x73\x74 as bytes";
+    // .. but no unicode escapes
+    // let escaped = b"\u{211D} is not allowed";
+    println!("Some escaped bytes: {:?}", escaped);
+
+    // Raw byte strings work just like raw strings
+    let raw_bytestrings = br"\u{211D} is not escaped here";
+    println!("{:?}", raw_bytestrings);
+
+    // Converting a byte array to `str` can fail
+    if let Ok(my_str) = str::from_utf8(raw_bytestrings) {
+        println!("And the same as text: '{}'", my_str);
+    }
+
+    let _quotes = br#"You can also use "fancier" formatting, \
+                    like with normal strings"#;
+
+    // Byte strings don't have to be UTF-8
+    let shift_jis = b"\x82\xe6\x82\xa8\x82\xb1\x82\xbb";
+    
+    // But then they can't always be converted to `str`
+    match str::from_utf8(shift_jis) {
+        Ok(my_str) => println!("Conversion successful: '{}'", my_str),
+        Err(e) => println!("Conversion failed: {:?}", e),
+    }
 }

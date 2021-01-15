8a39f1efaae610f93e07ec7eadf279a70aea7036
TanishqSingla <tanishqsingla00@gmail.com>
2021-01-15 18:48:06 +0530

    Added literals and escapes

--- 17.Std-library-types/strings.rs
+++ 17.Std-library-types/strings.rs
@@ -37,4 +37,23 @@ fn main() {
 
     println!("Alice says: {}", alice);
     println!("Bob says: {}", bob);
+
+
+    // You can use escapes to write bytes by their hexadecimal values...
+    let byte_escape = "I'm writing \x52\x75\x74!";
+    println!("What are you doing\x3F (\\x3F means?) {}", byte_escape);
+
+    // .. or Unicode points
+    let unicode_points = "\u{211D}";
+    let character_name = "\"DOUBLE-STRUCK CAPITAL R\"";
+
+    println!("Unicode character {} (U+211D) is called {}",
+                 unicode_points, character_name);
+
+    let long_strings = "String literals
+                         can span multiple lines.
+                         The linebreak and indentation here ->\
+                         <- can be escaped too!";
+    println!("{}", long_strings);
+
 }

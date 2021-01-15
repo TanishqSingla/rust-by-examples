e208005f1b1746b3816deff9c61051c8634fab6f
TanishqSingla <tanishqsingla00@gmail.com>
2021-01-15 13:20:19 +0530

    Added partition.rsc

---
+++ 16.Error-Handling/iterating-over-result/partition.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let strings = vec!["tofu", "93", "18"];
+    let (numbers, errors): (Vec<_>, Vec<_>) = strings
+        .into_iter()
+        .map(|s| s.parse::<i32>())
+        .partition(Result::is_ok);
+    println!("Numbers: {:?}", numbers);
+    println!("Errors: {:?}", errors);
+    // The above print statement output is wrapped in `Result`, to remove this
+    let numbers: Vec<_> = numbers.into_iter().map(Result::unwrap).collect();
+    let errors: Vec<_> = errors.into_iter().map(Result::unwrap_err).collect();
+    println!("Numbers: {:?}", numbers);
+    println!("Errors {:?}", errors);
+}

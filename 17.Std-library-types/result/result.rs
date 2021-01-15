ba388213c6ca151981fcd1c8739d837921839768
TanishqSingla <tanishqsingla00@gmail.com>
2021-01-15 19:26:32 +0530

    Added result

---
+++ 17.Std-library-types/result/result.rs
@@ -0,0 +1,57 @@
+mod checked {
+    // Mathematical "errors" we want to catch
+    #[derive(Debug)]
+    pub enum MathError {
+        DivisionByZero,
+        NonPositiveLogarithm,
+        NegativeSquareRoot,
+    }
+    
+    pub type MathResult = Result<f64, MathError>;
+
+    pub fn div(x: f64, y: f64) -> MathResult {
+        if y == 0.0 {
+            // This operation would `fail`, instead return the reason
+            Err(MathError::DivisionByZero)
+        } else {
+            Ok(x / y)
+        }
+    }
+
+    pub fn sqrt(x: f64) -> MathResult {
+        if x < 0.0 {
+            Err(MathError::NegativeSquareRoot)
+        } else {
+            Ok(x.sqrt())
+        }
+    }
+
+    pub fn ln(x: f64) -> MathResult {
+        if x <= 0.0 {
+            Err(MathError::NonPositiveLogarithm)
+        } else {
+            Ok(x.ln())
+        }
+    }
+}
+
+
+// op(x, y) == sqrt(ln(x, y))
+fn op(x: f64, y: f64) -> f64 {
+    // This is a three level match pyramid!
+    match checked::div(x, y) {
+        Err(why) => panic!("{:?}", why),
+        Ok(ratio) => match checked::ln(ratio) {
+            Err(why) => panic!("{:?}", why),
+            Ok(ln) => match checked::sqrt(ln) {
+                Err(why) => panic!("{:?}", why),
+                Ok(sqrt) => sqrt,
+            },
+        },
+    }
+}
+
+fn main() {
+    // Will this fail?
+    println!("{}", op(1.0, 1.0));
+}

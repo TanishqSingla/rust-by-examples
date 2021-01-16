c239de907b744461a2aba8208788cb6dd77dcd26
TanishqSingla <tanishqsingla00@gmail.com>
2021-01-17 01:04:57 +0530

    threads testcase map reduce added

---
+++ 18.Std-misc/threads-map-reduce.rs
@@ -0,0 +1,47 @@
+use std::thread;
+
+// This is the `main` thread
+fn main() {
+    // This is our data to process.
+    let data = "86967897737416471853297327050364959
+11861322575564723963297542624962850
+70856234701860851907960690014725639
+38397966707106094172783238747669219
+52380795257888236525459303330302837
+58495327135744041048897885734297812
+69920216438980873548808413720956532
+16278424637452589860345374828574668";
+
+    // vector to hold the child thread which we will spawn
+    let mut children = vec![];
+
+    // spliting data into segments for individual calculation
+    let chunked_data = data.split_whitespace();
+
+
+    for(i, data_segment) in chunked_data.enumerate() {
+        println!("data segment {} is \"{}\"", i, data_segment);
+
+        children.push(thread::spawn(move || -> u32 {
+            // Calculate the intermediate sum of this of our segment
+            let result = data_segment
+                        .chars()
+                        .map(|c| c.to_digit(10).expect("should be a digit"))
+                        .sum();
+            println!("processed segment {}, result={}", i, result);
+
+            result
+        }));
+    }
+
+    let mut intermediate_sums = vec![];
+
+    for child in children {
+        let intermediate_sum = child.join().unwrap();
+        intermediate_sums.push(intermediate_sum);
+    }
+
+    let final_result = intermediate_sums.iter().sum::<u32>();
+
+    println!("Final sum result: {}", final_result);
+}
